0726189a479fd99507094bb1f5e006aeb022a589
sergeykorneev1996 <korneevsk1996@gmail.com>
2020-07-29 18:00:51 +0300

    Corrected mistake: 1. register.html: 1.1. {% extends "learning_log/base.html %} to {% extends "learning_logs/base.html" %} 2. users\views.py: 2.1 context and return indentation

--- users/views.py
+++ users/views.py
@@ -26,5 +26,5 @@ def register(request):
             authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
             login(request, authenticated_user)
             return HttpResponseRedirect(reverse('index'))
-        context = {'form': form}
-        return render(request, 'users/register.html', context)
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
