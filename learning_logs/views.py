fcdabb71e3ba8eea1a0de87eb6886ec78f4461db
sergeykorneev1996 <korneevsk1996@gmail.com>
2020-07-31 20:30:38 +0300

    Refactoring: added function "check_topic_owner(topic, request)"

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -24,8 +24,7 @@ def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
     topic = Topic.objects.get(id=topic_id)
     # Проверка того, что тема принадлежит текущему пользователю
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -41,7 +40,9 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('topics'))
     
     context = {'form': form}
@@ -75,8 +76,7 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
 
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(topic, request)
 
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи
@@ -90,3 +90,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+
+def check_topic_owner(topic, request):
+    if topic.owner != request.user:
+        raise Http404
