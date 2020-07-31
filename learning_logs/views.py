268de52fcaf1ee85a062665c6e6f4d1d60c20df7
sergeykorneev1996 <korneevsk1996@gmail.com>
2020-07-31 19:48:38 +0300

    Restricted access in Learning Log

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -19,6 +19,7 @@ def topics(request):
     return render(request, 'learning_logs/topics.html', context)
 
 
+@login_required
 def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
     topic = Topic.objects.get(id=topic_id)
@@ -27,6 +28,7 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 
+@login_required
 def new_topic(request):
     """Определяет новую тему"""
     if request.method != 'POST':
@@ -43,6 +45,7 @@ def new_topic(request):
     return render(request, 'learning_logs/new_topic.html', context)
 
 
+@login_required
 def new_entry(request, topic_id):
     """Добавляет новую запись по конкретной теме"""
     topic = Topic.objects.get(id=topic_id)
@@ -63,6 +66,7 @@ def new_entry(request, topic_id):
     return render(request, 'learning_logs/new_entry.html', context)
 
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись"""
     entry = Entry.objects.get(id=entry_id)
